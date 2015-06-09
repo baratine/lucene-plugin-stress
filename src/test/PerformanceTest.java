673038b33bbee31fb8f9bd7ac7a5f4842be88166
alex <rojkov@gmail.com>
2015-06-09 10:22:36 -0700

    performance test work

--- src/test/PerformanceTest.java
+++ src/test/PerformanceTest.java
@@ -1,8 +1,9 @@
 package test;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Date;
+import java.io.PrintWriter;
 import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -11,54 +12,50 @@ import java.util.concurrent.Future;
 
 public class PerformanceTest
 {
-  private final int _preload;
   private final DataProvider _provider;
-  private final float _targetRatio;
   private SearchClient[] _clients;
 
-  public PerformanceTest(int c,
-                         int n,
-                         int preload,
-                         String host,
-                         int port,
-                         float targetRatio,
-                         DataProvider provider,
-                         ClientType type)
+  private Args _args;
+
+  public PerformanceTest(Args args)
     throws IOException, ExecutionException, InterruptedException
   {
-    Objects.requireNonNull(provider);
-    _preload = preload;
-    _provider = provider;
-    _targetRatio = targetRatio;
+    Objects.requireNonNull(args);
 
-    _clients = new SearchClient[c];
+    _args = args;
 
-    for (int i = 0; i < c; i++) {
-      SearchClient client = null;
+    long size = _args.c() * (_args.n() + _args.preload());
 
-      switch (type) {
+    _provider = new WikiDataProvider(args.getDataDir(), size);
+
+    _clients = new SearchClient[_args.c()];
+
+    for (int i = 0; i < _clients.length; i++) {
+      SearchClient client;
+
+      switch (args.type()) {
       case BRPC: {
         client = new BaratineRpc(_provider,
-                                 n,
-                                 targetRatio,
-                                 host,
-                                 port);
+                                 _args.n(),
+                                 _args.ratio(),
+                                 _args.host(),
+                                 _args.port());
         break;
       }
       case BRPC2: {
         client = new BaratineRpc2(_provider,
-                                  n,
-                                  targetRatio,
-                                  host,
-                                  port);
+                                  _args.n(),
+                                  _args.ratio(),
+                                  _args.host(),
+                                  _args.port());
         break;
       }
       case SOLR: {
         client = new Solr(_provider,
-                          n,
-                          targetRatio,
-                          host,
-                          port);
+                          _args.n(),
+                          _args.ratio(),
+                          _args.host(),
+                          _args.port());
         break;
       }
       default: {
@@ -75,7 +72,7 @@ public class PerformanceTest
     ExecutorService executors = Executors.newFixedThreadPool(_clients.length);
 
     for (SearchClient client : _clients) {
-      client.preload(_preload);
+      client.preload(_args.preload());
     }
 
     Future[] futures = new Future[_clients.length];
@@ -90,84 +87,170 @@ public class PerformanceTest
     executors.shutdown();
   }
 
-  public void printStats()
+  public void printStats() throws IOException
   {
-    int updates = 0;
-    int searches = 0;
-    long updateTime = 0;
-    long searchTime = 0;
-    for (SearchClient client : _clients) {
-      updates += client.getUpdateCount();
-      searches += client.getSearchCount();
+    try (PrintWriter writer
+           = new PrintWriter(new FileWriter(_args.getFile(), true), true)) {
 
-      updateTime = Math.max(updateTime, client.getUpdateTime());
-      searchTime = Math.max(searchTime, client.getSearchTime());
+      int updates = 0;
+      int searches = 0;
+      long updateTime = 0;
+      long searchTime = 0;
+      for (SearchClient client : _clients) {
+        updates += client.getUpdateCount();
+        searches += client.getSearchCount();
+
+        updateTime = Math.max(updateTime, client.getUpdateTime());
+        searchTime = Math.max(searchTime, client.getSearchTime());
+      }
+
+      writer.println(_args);
+      writer.println(
+        String.format(
+          "  submitted %1$d, searched %2$d, search-update-ratio %3$f, search-update-target %4$f",
+          updates,
+          searches,
+          ((float) searches / updates),
+          _args.ratio()));
+
+      writer.println(
+        String.format("  search avg: %1$f total-time: %2$d ops: %3$f",
+                      ((float) searchTime / searches), searchTime,
+                      ((float) searches / searchTime * 1000)));
+      writer.println(
+        String.format("  update avg: %1$f total-time: %2$d ops: %3$f",
+                      ((float) updateTime / updates), updateTime,
+                      ((float) updates / updateTime * 1000)));
+
+      writer.println();
     }
-
-    System.out.println(
-      String.format(
-        "  clients %1$d, submitted %2$d, searched %3$d, search-update-ratio %4$f, search-update-ratio-target %5$f",
-        _clients.length,
-        updates,
-        searches,
-        ((float) searches / updates),
-        _targetRatio));
-
-    System.out.println(
-      String.format("  search avg: %1$f total-time: %2$d ops: %3$f",
-                    ((float) searchTime / searches), searchTime,
-                    ((float) searches / searchTime * 1000)));
-    System.out.println(
-      String.format("  update avg: %1$f total-time: %2$d ops: %3$f",
-                    ((float) updateTime / updates), updateTime,
-                    ((float) updates / updateTime * 1000)));
   }
 
-  public static void main(String[] args)
+  public static void main(String[] vargs)
     throws IOException, ExecutionException, InterruptedException
   {
-    System.out.println("Start-Time: " + new Date());
-    File file = new File(args[0]);
+    Args args = new Args(vargs);
 
-    int c = 4;
-    int n = 1000;
-    int preload = 100;
-    String host = "localhost";
-    int port = 8085;
-    float targetRatio = 5;
-
-    if (!true) {
-      c = 4;
-      n = 400;
-      preload = 10;
-    }
-
-    int size = c * (n + preload);
-
-    DataProvider provider = new WikiDataProvider(file, size);
-
-    ClientType type = ClientType.BRPC2;
-    type = ClientType.BRPC;
-    //type = ClientType.SOLR;
-
-    PerformanceTest test = new PerformanceTest(c,
-                                               n,
-                                               preload,
-                                               host,
-                                               port,
-                                               targetRatio,
-                                               provider,
-                                               type);
-
-    System.out.println("Start-Time-Run : " + new Date());
+    PerformanceTest test = new PerformanceTest(args);
 
     test.run();
 
-    System.out.println("Finish-Time-Run: " + new Date());
-
-    System.out.println(type + ":");
-
     test.printStats();
+  }
+}
+
+class Args
+{
+  private int _c = 1;
+  private int _n = 1;
+  private int _pre = 1;
+  private String _host = "localhost";
+  private int _port = 8085;
+  private float _ratio = 5;
+  private ClientType _type = ClientType.BRPC2;
+  private File _dataDir;
+  private File _file;
+
+  Args(String[] args)
+  {
+    for (int i = 0; i < args.length; i++) {
+      String arg = args[i];
+
+      if ("-c".equals(arg)) {
+        _c = Integer.parseInt(args[++i]);
+      }
+      else if ("-n".equals(arg)) {
+        _n = Integer.parseInt(args[++i]);
+      }
+      else if ("-pre".equals(arg)) {
+        _pre = Integer.parseInt(args[++i]);
+      }
+      else if ("-host".equals(arg)) {
+        _host = args[++i];
+      }
+      else if ("-port".equals(arg)) {
+        _port = Integer.parseInt(args[++i]);
+      }
+      else if ("-ratio".equals(arg)) {
+        _ratio = Float.parseFloat(args[++i]);
+      }
+      else if ("-type".equals(arg)) {
+        _type = ClientType.valueOf(args[++i]);
+      }
+      else if ("-dir".equals(arg)) {
+        _dataDir = new File(args[++i]);
+      }
+      else if ("-file".equals(arg)) {
+        _file = new File(args[++i]);
+      }
+      else {
+        throw new IllegalStateException("unknown argument " + arg);
+      }
+    }
+
+    Objects.requireNonNull(_dataDir);
+
+    if (!_dataDir.isDirectory()) {
+      throw new IllegalStateException(_dataDir + " is not a directory");
+    }
+
+  }
+
+  public int c()
+  {
+    return _c;
+  }
+
+  public int n()
+  {
+    return _n;
+  }
+
+  public int preload()
+  {
+    return _pre;
+  }
+
+  public String host()
+  {
+    return _host;
+  }
+
+  public int port()
+  {
+    return _port;
+  }
+
+  public float ratio()
+  {
+    return _ratio;
+  }
+
+  public ClientType type()
+  {
+    return _type;
+  }
+
+  public File getDataDir()
+  {
+    return _dataDir;
+  }
+
+  public File getFile()
+  {
+    return _file;
+  }
+
+  @Override
+  public String toString()
+  {
+    return String.format("Args[%1$s, %2$d, %3$d, %4$d, %5$s, %6$d]",
+                         _type,
+                         _c,
+                         _n,
+                         _pre,
+                         _host,
+                         _port);
   }
 }
 
